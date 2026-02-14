576ede7188690e07e8df3b3d2e490ddc60888f9c
Francisco Carmona <flcarmonac@yahoo.com>
2026-02-14 20:10:09 -0300

    Update

--- src/app/shared/components/news-gallery-component/news-gallery-component.ts
+++ src/app/shared/components/news-gallery-component/news-gallery-component.ts
@@ -1,14 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { NewsGalleryModel } from '@core/models/news-gallery-model';
+import { ModalImageComponent } from "../modal-image-component/modal-image-component";
 
 @Component({
   selector: 'app-news-gallery-component',
   imports: [
     NgOptimizedImage,
-  ],
+    ModalImageComponent
+],
   templateUrl: './news-gallery-component.html',
 })
 export class NewsGalleryComponent {
   readonly newsGalleryList = input<NewsGalleryModel[]>([]);
+
+  readonly isImageModalOpen = signal(false);
+  readonly selectedImage = signal("");
 }
