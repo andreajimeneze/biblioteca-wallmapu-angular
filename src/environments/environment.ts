290135a23814de3fa9841a25c1cd49971983cda7
Andrea Jiménez Espinoza <121406808+andreajimeneze@users.noreply.github.com>
2026-02-14 00:55:42 -0300

    Switch API URL to biblioteca-wallmapu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
-  apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
+  apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
+  //apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
   googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com'
 };
