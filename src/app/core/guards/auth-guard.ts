adc0622073f70b0633673e654c2920879ec0bc17
Francisco Carmona <flcarmonac@yahoo.com>
2026-02-15 17:27:55 -0300

    Update

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,24 +1,5 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthStore } from '@features/auth/services/auth-store';
-import { ROUTES } from '@shared/constants/routes';
+import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const authStore = inject(AuthStore);
-  const router = inject(Router);
-  
-  const user = authStore.user(); // tu signal actual
-  if (!user) {
-    router.navigate([ROUTES.HOME]);
-    return false;
-  }
-
-  // Validación opcional de rol
-  const allowedRoles = route.data['roles'] as string[] | undefined;
-  if (allowedRoles && !allowedRoles.includes(user.role)) {
-    router.navigate([ROUTES.HOME]); // o '/unauthorized' si quieres
-    return false;
-  }
-  
   return true;
 };
