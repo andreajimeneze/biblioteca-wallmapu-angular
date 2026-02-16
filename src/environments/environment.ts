1a6dff945142558ff58311f76ebd6cbc3cae8dad
Andrea Jiménez Espinoza <121406808+andreajimeneze@users.noreply.github.com>
2026-02-16 15:13:27 -0300

    Switch API URLs in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //apiUrl: " http://127.0.0.1:8000/api",
-  apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
-  //apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
+  //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
+  apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
   googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com'
 };
