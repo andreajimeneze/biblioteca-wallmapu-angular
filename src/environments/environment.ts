465105b46ad3022b60e08bd3f9b218a3d1a879cd
Andrea Jiménez Espinoza <121406808+andreajimeneze@users.noreply.github.com>
2026-02-16 16:04:59 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: " http://127.0.0.1:8000/api",
+  //apiUrl: " http://127.0.0.1:8000/api",
   //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
   apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
   googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com'
