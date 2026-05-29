3a3847120c0343ec99f38a7ddf041ec977629a6b
Francisco Carmona <flcarmonac@yahoo.com>
2026-05-29 15:29:05 -0400

    fix: corregido typo "deceas" por "deseas" en modal de eliminar libro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://api-wallmapu-mesana.vercel.app/api',
   //apiUrl: "http://127.0.0.1:8000/api",
   apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
   googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com',
-  version: '1.0.252',
+  version: '1.0.253',
 };
