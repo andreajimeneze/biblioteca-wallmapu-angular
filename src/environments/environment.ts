04e6956b59dde6334be366b74632325c74d7ef7b
Andrea Jiménez Espinoza <andreacjimenez@gmail.com>
2026-06-12 11:31:37 -0400

    update google client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   apiUrl: 'https://biblioteca-wallmapu-python-zeta.vercel.app/api',
   //apiUrl: "http://127.0.0.1:3000/api",
   //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
-  googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com',
+  googleClientId: '1039052893034-aci663sargeat5mgae7f2st9qe8u8859.apps.googleusercontent.com',
   version: '1.0.262',
 };
