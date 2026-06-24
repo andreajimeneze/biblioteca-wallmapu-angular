29b70b13fa0b38bc3ed664afa9f43f39b9a024ca
Andrea Jiménez Espinoza <andreacjimenez@gmail.com>
2026-06-23 21:58:42 -0400

    conecta con api js

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  //apiUrl: "http://127.0.0.1:8000/api",
-  apiUrl: 'https://biblioteca-wallmapu-python-zeta.vercel.app/api',
+  apiUrl: "https://api-wallmapu-mesana.vercel.app/api",
+  //apiUrl: "http://127.0.0.1:3000/api",
+  //apiUrl: 'https://biblioteca-wallmapu-python-zeta.vercel.app/api',
   googleClientId: '1039052893034-aci663sargeat5mgae7f2st9qe8u8859.apps.googleusercontent.com', 
   version: '1.0.263',
 };
